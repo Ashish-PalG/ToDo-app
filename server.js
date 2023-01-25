9152e27561a2a6008df392917133f808209c27d4
ashish_pal <ashish_pal@turbohire.co>
2023-01-25 23:14:27 +0530

    base code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require('dotenv').config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("Server up and running"));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`);
+})
